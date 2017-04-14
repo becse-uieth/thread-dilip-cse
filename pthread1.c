d6dcb3b87557a9949652bf1918dde78ea45982a5
dilip-cse <krdilip276@gmail.com>
2017-04-14 16:54:39 +0530

    posix_thread_1st programme
    this is the class illustration.
    under Linux Administration and Programming.

---
+++ pthread1.c
@@ -0,0 +1,22 @@
+#include<pthread.h>
+#include<stdio.h>
+
+void *add(void *a);
+int main(){
+	void *c;
+	pthread_t tid;
+	int a[]={1,2};
+	pthread_create(&tid,NULL,&add,a);
+	pthread_join(tid,(void*)&c);
+	int d = (int)c;
+	printf("%d\n",c);
+	return 0;
+}
+void *add(void *a)
+{
+int *b;
+int sum;
+b=(int*)a;
+sum=b[0]+b[1];
+return (void*) sum;
+}
